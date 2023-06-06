88b97abb4b054a9a64c92746bc1b0c5a220440c0
Omar Alzoubi <118571302+OmarAz01@users.noreply.github.com>
2023-06-05 20:13:18 -0500

    Animations and Posting Feature
    Added animations to the feed and included the ability to add skills and levels to post

--- devsync-frontend/src/components/GetPosts.jsx
+++ devsync-frontend/src/components/GetPosts.jsx
@@ -3,8 +3,7 @@ import axios from 'axios';
 import Image from 'react-bootstrap/Image';
 import pp1Image from '../assets/pp1.jpg';
 
-
-const GetPosts = () => {
+const GetPosts = ({ createAlert }) => {
   const BASE_URL = 'http://localhost:8080';
   const [posts, setPosts] = useState([]);
   const [visiblePosts, setVisiblePosts] = useState(5);
@@ -14,7 +13,9 @@ const GetPosts = () => {
   useEffect(() => {
     const getPosts = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/posts/all`);
+        const response = await axios.get(
+          `${BASE_URL}/api/posts/all`
+        );
         setPosts(response.data);
         fetchUsersForPosts(response.data);
       } catch (error) {
@@ -25,32 +26,38 @@ const GetPosts = () => {
     getPosts();
   }, []);
 
-  const fetchUsersForPosts = async (posts) => {
-
-    const userIds = posts.map((post) => post.userId);
+  // Fetches all the users of the posts
+  const fetchUsersForPosts = async posts => {
+    const userIds = posts.map(post => post.userId);
 
     try {
-      const response = await axios.post(`${BASE_URL}/api/user/userforpost`, { userIds });
+      const response = await axios.post(
+        `${BASE_URL}/api/user/userforpost`,
+        { userIds }
+      );
       setUsers(response.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const fetchUserForPost = (id) => {
-    const user = users.find((user) => user.userId === id);
+  // Fetches the one user for the post
+  const fetchUserForPost = id => {
+    const user = users.find(user => user.userId === id);
 
     return user;
   };
 
   const handleMorePosts = () => {
-    setVisiblePosts((prevVisiblePosts) => prevVisiblePosts + 10);
+    setVisiblePosts(prevVisiblePosts => prevVisiblePosts + 10);
   };
 
-  const handleDelete = async (postId) => {
+  const handleDelete = async postId => {
     try {
       await axios.delete(`${BASE_URL}/api/posts/delete/${postId}`);
+      createAlert('Post deleted', 'success');
     } catch (error) {
+      createAlert('Error deleting post', 'error');
       console.log(error);
     }
 
@@ -61,60 +68,112 @@ const GetPosts = () => {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  const formateTime = (minutes) => {
+  const formateTime = minutes => {
     if (minutes < 1) {
-      return 'less than 1 minutes ago';
-    }
-    else if (minutes < 60) {
+      return 'less than 1 minute ago';
+    } else if (minutes < 60) {
       return minutes + ' minutes ago';
-    }
-    else if (minutes < 1440) {
+    } else if (minutes < 120) {
+      return Math.floor(minutes / 60) + ' hour ago';
+    } else if (minutes < 1440) {
       return Math.floor(minutes / 60) + ' hours ago';
-    }
-    else {
+    } else if (minutes < 2880) {
+      return Math.floor(minutes / 1440) + ' day ago';
+    } else {
       return Math.floor(minutes / 1440) + ' days ago';
     }
-  }
+  };
+
+  // To be implemented
+  const handleEdit = postId => {};
 
   return (
     <>
-      {posts.slice(0, visiblePosts).map((post) => {
+      {posts.slice(0, visiblePosts).map(post => {
         const postUser = fetchUserForPost(post.userId);
         const submittedDate = new Date(post.dateCreated);
-        const currentDate = new Date();
-        const timeDiff = Math.abs(currentDate.getTime() - submittedDate.getTime());
+        const currentDate = new Date(
+          new Date().toLocaleString('en-US', {
+            timeZone: 'America/New_York'
+          })
+        );
+        const timeDiff = Math.abs(
+          currentDate.getTime() - submittedDate.getTime()
+        );
         const minutes = Math.floor(timeDiff / 60000);
 
         return (
-          <div key={post.postId} className='pt-4 pb-6 border rounded-lg shadow-md border-black my-4 min-w-full 
-          max-w-screen-md bg-zinc-800 flex flex-row'> 
-            <div className='flex flex-col text-center min-w-fit'>
-              <Image src={pp1Image} alt='image' className='md:w-20 md:h-20 w-12 h-12 mx-4 mb-2 rounded-full' />
-              <h4 className='md:text-base text-xs'> {postUser ? '@' + postUser.username : null} </h4>
-              <h4 className='md:text-base text-xs'> {postUser ? postUser.level : null} </h4>
-              {post.userId === currUser.id ? <h4 className='md:text-base text-xs'> <button onClick={() => handleDelete(post.postId)} className='bg-red-600 mt-2 
-              px-2 hover:bg-red-500'>Delete</button> </h4> : null}
+          <div
+            key={post.postId}
+            className="pt-4 pb-6 border rounded-lg shadow-md border-black my-4 min-w-full 
+          max-w-screen-md bg-zinc-800 flex flex-row">
+            <div className="flex flex-col text-center justify-center items-center min-w-fit">
+              <Image // User Information
+                src={pp1Image}
+                alt="image"
+                className="md:w-20 md:h-20 w-12 h-12 mx-4 mb-2 rounded-full"
+              />
+              <h4 className="md:text-base text-xs">
+                {' '}
+                {postUser ? '@' + postUser.username : null}{' '}
+              </h4>
+              <h4 className="md:text-base text-xs">
+                {' '}
+                {postUser ? postUser.level : null}{' '}
+              </h4>
+              {post.userId === currUser.userId ? (
+                <>
+                  <button
+                    onClick={() => {
+                      handleDelete(post.postId);
+                    }}
+                    className="bg-red-600 mt-2 
+              px-2 hover:bg-red-500 rounded-md w-18 md:text-base text-xs">
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => handleEdit(post.postId)}
+                    className="bg-blue-600 mt-2 
+              px-4 hover:bg-blue-500 rounded-md md:text-base text-xs">
+                    Edit
+                  </button>
+                </>
+              ) : null}
             </div>
-            <div className='flex flex-col w-4/5 max-w-screen-sm mx-4 relative border-white rounded-lg bg-zinc-700'>
-              <p className='text-sm md:text-base px-4 py-2 break-all mb-8'>{post.content}</p>
-              <div className='flex flex-row justify-between'>
-                <h3 className='text-sm md:text-base pt-6 px-4 absolute bottom-2'> Skills Needed: {postUser ? post.skillNeeded : null}</h3>
-          
+            <div className="flex flex-col w-11/12 max-w-screen-sm relative rounded-lg mr-2 bg-zinc-700">
+              <p className="text-sm md:text-base px-4 py-2 break-all mb-8">
+                {post.content}
+              </p>
+              <div className="flex flex-row justify-between">
+                <h3 className="text-sm md:text-base pt-6 px-4 absolute bottom-2">
+                  {' '}
+                  Skills Needed: {postUser ? post.skillNeeded : null}
+                </h3>
               </div>
-              <div className='flex absolute -bottom-5 right-0'>                
-                <h4 className='text-xs opacity-60 italic'> {formateTime(minutes)} </h4> 
+              <div className="flex absolute -bottom-5 right-0">
+                <h4 className="text-xs opacity-60 italic">
+                  {' '}
+                  {formateTime(minutes)}{' '}
+                </h4>
               </div>
-            </div>      
-            
+            </div>
           </div>
         );
       })}
 
       {visiblePosts < posts.length ? (
-        <button className='hover:text-zinc-500' onClick={handleMorePosts}>Load More...</button>
-      ) : <h4 className='text-sm md:text-base text-center py-4'>No more posts to show</h4>}
+        <button
+          className="hover:text-zinc-500"
+          onClick={handleMorePosts}>
+          Load More...
+        </button>
+      ) : (
+        <h4 className="text-sm md:text-base text-center py-4">
+          No more posts to show
+        </h4>
+      )}
     </>
   );
 };
--- devsync-frontend/src/pages/Feed.jsx
+++ devsync-frontend/src/pages/Feed.jsx
@@ -1,39 +1,73 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import GetPosts from '../components/GetPosts'
-import SubmitPost from '../components/SubmitPost'
+import React, { useEffect, useState } from 'react';
+import GetPosts from '../components/GetPosts';
+import SubmitPost from '../components/SubmitPost';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Feed = () => {
-    const user = JSON.parse(localStorage.getItem('user'))
-    const BASE_URL = 'http://localhost:8080'
-    const [show, setShow] = useState(false)
+  const user = JSON.parse(localStorage.getItem('user'));
+  const BASE_URL = 'http://localhost:8080';
+  const [show, setShow] = useState(false);
+
+  const createAlert = (title, variant) => {
+    if (variant === 'success') {
+      return toast.success(title);
+    } else {
+      return toast.error(title);
+    }
+  };
 
   return (
     <>
-        <div className="flex flex-col justify-center items-center">
-            <div className="flex flex-col w-full text-center justify-center max-w-screen-xl">   
-                    <div className='flex flex-col max-w-screen-lg py-8 text-left'>
-                        {show ?
-                        <>
-                        <div className='absolute md:ml-72 ml-52'>
-                            <h4 onClick={() => setShow(!show)} className='hover:text-gray-400 md:text-3xl 
-                            h-auto text-2xl py-8 md:px-8 px-4 underline hover:cursor-pointer'>Cancel</h4>
-                        </div>
-                        <SubmitPost />                        
-                        </>
-                        : 
-                        <h4 onClick={() => setShow(!show)} className='hover:text-gray-400 md:text-3xl text-2xl
-                         py-8 md:px-8 px-4 underline hover:cursor-pointer w-56'>Start a post</h4>}  
-                         <h4 className='text-3xl md:pl-8 pl-4 py-8'>Posts</h4>                    
-                    </div>     
-                    <div className='flex flex-col max-w-screen-md pb-8 text-left items-center mx-4 md:mx-8'>                          
-                        <GetPosts />
-                    </div>  
-            </div>   
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col w-full text-center justify-center max-w-screen-xl">
+          <div className="flex flex-col max-w-screen-lg py-8 text-left">
+            <div
+              className={`${
+                show
+                  ? 'absolute w-full max-w-full transition-all opacity-100 translate-y-0 duration-500 delay-200 ease-in-out'
+                  : '-translate-y-96 pointer-events-none opacity-0 transition-all duration-500 ease-in-out h-0 delay-200 overflow-visible'
+              }`}>
+              <SubmitPost
+                setShow={setShow}
+                createAlert={createAlert}
+              />
+            </div>
+            <h4
+              onClick={() => setShow(!show)}
+              className={`${
+                show
+                  ? 'opacity-0 pointer-events-none transition-opacity'
+                  : 'transition-opacity opacity-100 duration-300 delay-500'
+              } hover:text-gray-400 md:text-3xl text-2xl py-8 md:px-8 px-4 underline hover:cursor-pointer w-56`}>
+              Start a post
+            </h4>
+          </div>
+          <div
+            className={`${
+              show
+                ? 'transition-all duration-500 ease-in-out md:translate-y-1/2 translate-y-96'
+                : 'translate-y-0 transition-all duration-500 ease-in-out'
+            } transition-transform translate-y-0 duration-1000 ease-in-out flex flex-col max-w-screen-md pb-8 text-left items-center mx-4 md:mx-8`}>
+            <h4 className="text-3xl md:pl-8 pl-4 py-8 ">Posts</h4>
+            <GetPosts createAlert={createAlert} />
+          </div>
         </div>
-    
+      </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
